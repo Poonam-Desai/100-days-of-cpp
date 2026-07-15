12d43676295394a174cc84d7853419e71afc79bb
Poonam Desai <poonamdesai0602@gmail.com>
2026-07-15 23:20:06 +0530

    Create evenodd.cpp

---
+++ evenodd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    if(num % 2 == 0) {
+        cout << num << " is Even Number" << endl;
+    } 
+    else {
+        cout << << num << " is Odd Number" << endl;
+    }
+    
+    return 0;
+}
